f9f3e3911bd82cef16d841649a3d5de1351eba6c
Sokol Paja <kolpaja28@gmail.com>
2024-09-17 17:59:50 +0200

    check memory leak

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	// other Webpack settings
+	optimization: {
+		minimize: true, // enables minimization
+		minimizer: [
+			// If you're using CSS minimizers like cssnano or Css Minimizer Plugin
+		],
+	},
+};
